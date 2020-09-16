85d306dec9656ac8edb36b1b15ae977e613ec80b
stocksd <v@stocksd.app>
2020-09-16 14:34:05 +0300

    [TEST] Switch to new token and tidy up formatting

--- test/e2e.js
+++ test/e2e.js
@@ -1,7 +1,7 @@
 const constants = require('./consts.js');
 const utils = require('./utils.js');
 
-const SToken = artifacts.require("SToken");
+const STO = artifacts.require("STO");
 const S = artifacts.require("S");
 const DAItokenAddress = "0xc2118d4d90b274016cB7a54c03EF52E6c537D957";
 const LINKtokenAddress = "0x20fe562d797a42dcb3399062ae9546cd06f63280";
@@ -11,9 +11,10 @@ const tickerJobId = web3.utils.fromAscii("51df1946d454408b90f15530d35c134a");
 
 // NOTE E2E test is tied to Ropsten oracles
 contract('SToken', (accounts) => {
-  it('Setup: set exchange in token, token in exchange, oracle', async () => {
+
+  it('Setup: set STO and oracle addresses in exchange, smoke check', async () => {
     //boilerplate
-    const STokenInstance = await SToken.deployed();
+    const STokenInstance = await STO.deployed();
     const SInstance = await S.deployed();
     const balance = await STokenInstance.balanceOf.call(accounts[0]);
     const tokenAddress = STokenInstance.address.toString();
@@ -21,7 +22,6 @@ contract('SToken', (accounts) => {
     await SInstance.setSToken(tokenAddress, { from: accounts[0] });
     const stokenInS = await SInstance.SToken.call();
     await SInstance.setDAIToken(DAItokenAddress, { from: accounts[0] });
-    await STokenInstance.setExchange(exchangeAddress, { from: accounts[0] });
     await SInstance.setOracle(oracleAddress, true, { from: accounts[0] });
     const DAI_token = await SInstance.DAI_token.call();
     //log
@@ -34,11 +34,13 @@ contract('SToken', (accounts) => {
       web3.utils.toWei("100", 'mether'),
       "unexpected value in the first account");
   });
+
   it('Approvals', async () => {
     const DAIContract = new web3.eth.Contract(constants.DAIABI, DAItokenAddress);
     await DAIContract.methods.approve(exchangeAddress, web3.utils.toWei("99999999999", 'tether')).send({ from: accounts[0] });
     await DAIContract.methods.approve(exchangeAddress, web3.utils.toWei("99999999999", 'tether')).send({ from: accounts[1] });
   });
+
   it('Send some LINK to cover price of all requests', async () => {
     //boilerplate
     const LINKContract = new web3.eth.Contract(constants.LINKABI, LINKtokenAddress);
@@ -49,17 +51,17 @@ contract('SToken', (accounts) => {
       web3.utils.toWei("10", 'ether'),
       "sending link to exchange failed");
   });
-  it('Set up a new dsellerage offer for TSLA', async () => {
+
+  it('Add new offers', async () => {
     //boilerplate
     const SInstance = await S.deployed();
-
     async function newOffer(price, collateral, ticker, sCollateral, cut, afkHours, ms) {
       await SInstance.createDsellerOffer(price, collateral, ticker, web3.utils.fromAscii(ticker),
         web3.utils.toWei(sCollateral, 'ether'),
         cut, utils.getAfkHours(afkHours), ms, { from: accounts[0] });
     }
-    //same thing from remix
-    //2000, 50, "TSLA", "0x54534c41", 50000000000000000000, 105, [], 0
+
+    //Remix UI translation: 2000, 50, "TSLA", "0x54534c41", 50000000000000000000, 105, [], 0
     await newOffer(2000, 50, "TSLA", "50", 105, [], 0);
     //more trades that won't be accepted
     await newOffer(3000, 100, "MSFT", "20", 110, [], 0,);
@@ -71,7 +73,8 @@ contract('SToken', (accounts) => {
     const resTrade = await SInstance.trades.call(0);
     assert.equal(resTrade.seller, accounts[0], "failed to create the offer");
   });
-  it('Send some Stokens to cover request price', async () => {
+
+  it('Send some STO to cover oracle request expenses', async () => {
     //boilerplate
     const STokenInstance = await SToken.deployed();
     await STokenInstance.transfer(accounts[1], web3.utils.toWei("1000", 'ether'), { from: accounts[0] });
@@ -81,7 +84,8 @@ contract('SToken', (accounts) => {
       web3.utils.toWei("1000", 'ether'),
       "pay-for-request transfer failed");
   });
-  it('Accept dseller offer', async function() {
+
+  it('Accept first offer', async function() {
     //boilerplate
     const SInstance = await S.deployed();
     await SInstance.
@@ -98,6 +102,5 @@ contract('SToken', (accounts) => {
       assert.equal(tradeIsActive, true, "failed to activate the trade");
     }, 120000)
   });
-  //this should be optimized
-});
 
+});
